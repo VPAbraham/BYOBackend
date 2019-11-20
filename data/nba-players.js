5dc62140779dee4c3e6ab3be8f9afc4b375b317f
VPAbraham <vpa456@gmail.com>
2019-11-20 15:26:09 -0700

    Refactor data from CSV to JSON

---
+++ data/nba-players.js
@@ -0,0 +1,244 @@
+const playerData = [
+  {
+    "Id": 1,
+    "Team": "Celtics",
+    "Abbreviation": "Bos",
+    "City": "Boston",
+    "State": "Massachusetts",
+    "Venue": "TD Garden"
+  },
+  {
+    "Id": 2,
+    "Team": "Nets",
+    "Abbreviation": "Bro",
+    "City": "Brooklyn",
+    "State": "New York",
+    "Venue": "Barclays Center"
+  },
+  {
+    "Id": 3,
+    "Team": "Knicks",
+    "Abbreviation": "Nyk",
+    "City": "New York",
+    "State": "New York",
+    "Venue": "Madison Square Garden"
+  },
+  {
+    "Id": 4,
+    "Team": "76ers",
+    "Abbreviation": "Phi",
+    "City": "Philadelphia",
+    "State": "Pennsylvania",
+    "Venue": "Wells Fargo Center"
+  },
+  {
+    "Id": 5,
+    "Team": "Raptors",
+    "Abbreviation": "Tor",
+    "City": "Toronto",
+    "State": "Ontario",
+    "Venue": "Air Canada Centre"
+  },
+  {
+    "Id": 6,
+    "Team": "Bulls",
+    "Abbreviation": "Chi",
+    "City": "Chicago",
+    "State": "Illinois",
+    "Venue": "United Center"
+  },
+  {
+    "Id": 7,
+    "Team": "Cavaliers",
+    "Abbreviation": "Cle",
+    "City": "Cleveland",
+    "State": "Ohio",
+    "Venue": "Quicken Loans Arena"
+  },
+  {
+    "Id": 8,
+    "Team": "Pistons",
+    "Abbreviation": "Det",
+    "City": "Detriot",
+    "State": "Michigan",
+    "Venue": "The Palace of Auburn Hills"
+  },
+  {
+    "Id": 9,
+    "Team": "Pacers",
+    "Abbreviation": "Ind",
+    "City": "Indianapolis",
+    "State": "Indiana",
+    "Venue": "Bankers Life Fieldhouse"
+  },
+  {
+    "Id": 10,
+    "Team": "Bucks",
+    "Abbreviation": "Mil",
+    "City": "Milwaukee",
+    "State": "Wisconsin",
+    "Venue": "Bradley Center"
+  },
+  {
+    "Id": 11,
+    "Team": "Hawks",
+    "Abbreviation": "Atl",
+    "City": "Atlanta",
+    "State": "Georgia",
+    "Venue": "Philips Arena"
+  },
+  {
+    "Id": 12,
+    "Team": "Hornets",
+    "Abbreviation": "Cha",
+    "City": "Charlotte",
+    "State": "North Carolina",
+    "Venue": "Time Warner Cable Arena"
+  },
+  {
+    "Id": 13,
+    "Team": "Heat",
+    "Abbreviation": "Mia",
+    "City": "Miami",
+    "State": "Florida",
+    "Venue": "AmericanAirlines Arena"
+  },
+  {
+    "Id": 14,
+    "Team": "Magic",
+    "Abbreviation": "Orl",
+    "City": "Orlando",
+    "State": "Florida",
+    "Venue": "Amway Center"
+  },
+  {
+    "Id": 15,
+    "Team": "Wizards",
+    "Abbreviation": "Was",
+    "City": "Washington",
+    "State": "D.C.",
+    "Venue": "Verizon Center"
+  },
+  {
+    "Id": 16,
+    "Team": "Nuggets",
+    "Abbreviation": "Den",
+    "City": "Denver",
+    "State": "Colorado",
+    "Venue": "Pepsi Center"
+  },
+  {
+    "Id": 17,
+    "Team": "Timberwolves",
+    "Abbreviation": "Min",
+    "City": "Minneapolis",
+    "State": "Minnesota",
+    "Venue": "Target Center"
+  },
+  {
+    "Id": 18,
+    "Team": "Thunder",
+    "Abbreviation": "Okc",
+    "City": "Oklahoma City",
+    "State": "Oklahoma",
+    "Venue": "Chesapeake Energy Arena"
+  },
+  {
+    "Id": 19,
+    "Team": "Trail Blazers",
+    "Abbreviation": "Por",
+    "City": "Portland",
+    "State": "Oregon",
+    "Venue": "Rose Garden"
+  },
+  {
+    "Id": 20,
+    "Team": "Jazz",
+    "Abbreviation": "Uta",
+    "City": "Salt Lake City",
+    "State": "Utah",
+    "Venue": "EnergySolutions Arena"
+  },
+  {
+    "Id": 21,
+    "Team": "Warriors",
+    "Abbreviation": "Gol",
+    "City": "Oakland",
+    "State": "California",
+    "Venue": "Oracle Arena"
+  },
+  {
+    "Id": 22,
+    "Team": "Clippers",
+    "Abbreviation": "Lac",
+    "City": "Los Angeles",
+    "State": "California",
+    "Venue": "Staples Center"
+  },
+  {
+    "Id": 23,
+    "Team": "Lakers",
+    "Abbreviation": "Lal",
+    "City": "Los Angeles",
+    "State": "California",
+    "Venue": "Staples Center"
+  },
+  {
+    "Id": 24,
+    "Team": "Suns",
+    "Abbreviation": "Pho",
+    "City": "Phoenix",
+    "State": "Arizona",
+    "Venue": "US Airways Center"
+  },
+  {
+    "Id": 25,
+    "Team": "Kings",
+    "Abbreviation": "Sac",
+    "City": "Sacramento",
+    "State": "California",
+    "Venue": "Power Balance Pavilion"
+  },
+  {
+    "Id": 26,
+    "Team": "Mavericks",
+    "Abbreviation": "Dal",
+    "City": "Dallas",
+    "State": "Texas",
+    "Venue": "American Airlines Center"
+  },
+  {
+    "Id": 27,
+    "Team": "Rockets",
+    "Abbreviation": "Hou",
+    "City": "Houston",
+    "State": "Texas",
+    "Venue": "Toyota Center"
+  },
+  {
+    "Id": 28,
+    "Team": "Grizzlies",
+    "Abbreviation": "Mem",
+    "City": "Memphis",
+    "State": "Tennessee",
+    "Venue": "FedExForum"
+  },
+  {
+    "Id": 29,
+    "Team": "Pelicans",
+    "Abbreviation": "Nor",
+    "City": "New Orleans",
+    "State": "Louisiana",
+    "Venue": "New Orleans Arena"
+  },
+  {
+    "Id": 30,
+    "Team": "Spurs",
+    "Abbreviation": "San",
+    "City": "San Antonio",
+    "State": "Texas",
+    "Venue": "AT&T Center"
+  }
+]
+
+export default playerData;
