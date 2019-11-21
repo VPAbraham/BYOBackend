23ec1d2222c3ca0dadeb36ba5a6cd1ea23dc5988
VPAbraham <vpa456@gmail.com>
2019-11-20 22:21:06 -0700

    Add babel and get endpoint for all players

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const express = require('express');
-const app = express();
+// import playerData from './data/nba-players';
+import teamData from './data/nba-teams';
+import express from 'express';
 
+// const express = require('express');
+const app = express();
+// console.log(teamData)
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.json());
+app.use(express.static('public'))
 
 app.locals.title = 'NBAPI';
 
 app.get('/', (request, response) => {
-  response.send('BALLLLLLINNNN');
+  response.send(`${teamData[0]['FULL NAME']}`);
 });
 
 app.listen(app.get('port'), () => {
-  console.log('its running')
+  console.log(`${app.locals.title} is running on http://localhost${app.get('port')}`)
 });
