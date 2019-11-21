28df7caea9e0a65d50151f70fd20032cd7fdda38
VPAbraham <vpa456@gmail.com>
2019-11-20 22:26:32 -0700

    Add endpoint for teams GET and fix some issues with mismatched data naming

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-// import playerData from './data/nba-players';
+import playerData from './data/nba-players';
 import teamData from './data/nba-teams';
 import express from 'express';
 
-// const express = require('express');
 const app = express();
-// console.log(teamData)
-app.set('port', process.env.PORT || 3000);
 
+app.set('port', process.env.PORT || 3000);
 app.use(express.json());
-app.use(express.static('public'))
 
 app.locals.title = 'NBAPI';
+app.locals.teams = teamData;
+app.locals.players = playerData;
 
 app.get('/', (request, response) => {
   response.send(`${teamData[0]['FULL NAME']}`);
 });
+
+app.get('/api/v1/players', (request, response) => {
+  const { players } = app.locals;
+  console.log('2018-2019 NBA Player Stats', players.length)
+  response.json({ players });
+})
+
+app.get('/api/v1/teams', (request, response) => {
+  const { teams } = app.locals;
+  console.log('NBA Teams', teams.length)
+  response.json({ teams });
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost${app.get('port')}`)
