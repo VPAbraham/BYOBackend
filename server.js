c254db0465642c08b4f38ce92b44ad113a18e1b1
VPAbraham <vpa456@gmail.com>
2019-11-25 01:11:00 -0700

    Add get method for getting player by id

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-// import playerData from './data/nba-teams';
-// import teamData from './data/nba-players';
-// import express from 'express';
 const playerData = require('./data/nba-teams')
 const teamData = require('./data/nba-players')
 const express = require('express');
-
+const cors = require('cors');
+const app = express();
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
-const cors = require('cors');
-const app = express();
+
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
@@ -20,9 +17,10 @@ app.locals.teams = teamData;
 app.locals.players = playerData;
 
 app.get('/', (request, response) => {
-  response.send('Welcome to NBAPI 2018-2019');
+  response.send('Endpoints available at https://github.com/VPAbraham/BYOBackend');
 });
 
+//GET all players
 app.get('/api/v1/players', (request, response) => {
   database('players')
   .select()
@@ -31,19 +29,57 @@ app.get('/api/v1/players', (request, response) => {
   })
   .catch((error) => {
     response.status(500).json({ error: "Unable to retrieve player data" });
-  })
-})
+  });
+});
 
+//GET all teams
 app.get('/api/v1/teams', (request, response) => {
   database('players').select()
     .then((players) => {
       response.status(200).json(players)
     })
     .catch((error) => {
-      response.status(500).json({ error: "Unable to retrieve player data" });
-    })
+      response.status(500).json({error});
+    });
+});
 
-})
+//GET players by id
+app.get('/api/v1/players/:id', (request, response) => {
+  const { id } = request.params;
+  database('players').select()
+  .where({id: id})
+  .then((player) => {
+    if (player.length === 0) {
+      response.status(404).json('There is no player with that id.')
+    } else {
+      response.status(404).json(player);
+    };
+  })
+  .catch((error) => {
+    response.status(500).json({ error });
+  });
+});
+
+//GET teams by id
+app.get('/api/v1/teams/:id', (request, response) => {
+  const { id } = request.params;
+  database('teams').select()
+    .where({id: id})
+    .then((team) => {
+      if (team.length === 0) {
+        request.status(404).json('No match for that team.');
+      } else {
+        response.status(200).json(team);
+      };
+    })
+    .catch((error) => {
+      response.status(500).json({error});
+    });
+});
+
+
+
+
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}`)
