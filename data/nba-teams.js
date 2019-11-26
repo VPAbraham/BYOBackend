8d07a7239466a4bd8f64c1aec65de5e75b3041ad
VPAbraham <vpa456@gmail.com>
2019-11-26 13:53:14 -0700

    Change datasets to incorporate a raltionship between teamId integers rather than string names

--- data/nba-teams.js
+++ data/nba-teams.js
@@ -1,214 +1,242 @@
-const teamsData = [
+[
   {
     "Team": "Celtics",
     "Abbreviation": "Bos",
     "City": "Boston",
     "State": "Massachusetts",
-    "Venue": "TD Garden"
+    "Venue": "TD Garden",
+    "Id": 1
   },
   {
     "Team": "Nets",
     "Abbreviation": "Bro",
     "City": "Brooklyn",
     "State": "New York",
-    "Venue": "Barclays Center"
+    "Venue": "Barclays Center",
+    "Id": 2
   },
   {
     "Team": "Knicks",
     "Abbreviation": "Nyk",
     "City": "New York",
     "State": "New York",
-    "Venue": "Madison Square Garden"
+    "Venue": "Madison Square Garden",
+    "Id": 3
   },
   {
     "Team": "76ers",
     "Abbreviation": "Phi",
     "City": "Philadelphia",
     "State": "Pennsylvania",
-    "Venue": "Wells Fargo Center"
+    "Venue": "Wells Fargo Center",
+    "Id": 4
   },
   {
     "Team": "Raptors",
     "Abbreviation": "Tor",
     "City": "Toronto",
     "State": "Ontario",
-    "Venue": "Air Canada Centre"
+    "Venue": "Air Canada Centre",
+    "Id": 5
   },
   {
     "Team": "Bulls",
     "Abbreviation": "Chi",
     "City": "Chicago",
     "State": "Illinois",
-    "Venue": "United Center"
+    "Venue": "United Center",
+    "Id": 6
   },
   {
     "Team": "Cavaliers",
     "Abbreviation": "Cle",
     "City": "Cleveland",
     "State": "Ohio",
-    "Venue": "Quicken Loans Arena"
+    "Venue": "Quicken Loans Arena",
+    "Id": 7
   },
   {
     "Team": "Pistons",
     "Abbreviation": "Det",
     "City": "Detriot",
     "State": "Michigan",
-    "Venue": "The Palace of Auburn Hills"
+    "Venue": "The Palace of Auburn Hills",
+    "Id": 8
   },
   {
     "Team": "Pacers",
     "Abbreviation": "Ind",
     "City": "Indianapolis",
     "State": "Indiana",
-    "Venue": "Bankers Life Fieldhouse"
+    "Venue": "Bankers Life Fieldhouse",
+    "Id": 9
   },
   {
     "Team": "Bucks",
     "Abbreviation": "Mil",
     "City": "Milwaukee",
     "State": "Wisconsin",
-    "Venue": "Bradley Center"
+    "Venue": "Bradley Center",
+    "Id": 10
   },
   {
     "Team": "Hawks",
     "Abbreviation": "Atl",
     "City": "Atlanta",
     "State": "Georgia",
-    "Venue": "Philips Arena"
+    "Venue": "Philips Arena",
+    "Id": 11
   },
   {
     "Team": "Hornets",
     "Abbreviation": "Cha",
     "City": "Charlotte",
     "State": "North Carolina",
-    "Venue": "Time Warner Cable Arena"
+    "Venue": "Time Warner Cable Arena",
+    "Id": 12
   },
   {
     "Team": "Heat",
     "Abbreviation": "Mia",
     "City": "Miami",
     "State": "Florida",
-    "Venue": "AmericanAirlines Arena"
+    "Venue": "AmericanAirlines Arena",
+    "Id": 13
   },
   {
     "Team": "Magic",
     "Abbreviation": "Orl",
     "City": "Orlando",
     "State": "Florida",
-    "Venue": "Amway Center"
+    "Venue": "Amway Center",
+    "Id": 14
   },
   {
     "Team": "Wizards",
     "Abbreviation": "Was",
     "City": "Washington",
     "State": "D.C.",
-    "Venue": "Verizon Center"
+    "Venue": "Verizon Center",
+    "Id": 15
   },
   {
     "Team": "Nuggets",
     "Abbreviation": "Den",
     "City": "Denver",
     "State": "Colorado",
-    "Venue": "Pepsi Center"
+    "Venue": "Pepsi Center",
+    "Id": 16
   },
   {
     "Team": "Timberwolves",
     "Abbreviation": "Min",
     "City": "Minneapolis",
     "State": "Minnesota",
-    "Venue": "Target Center"
+    "Venue": "Target Center",
+    "Id": 17
   },
   {
     "Team": "Thunder",
     "Abbreviation": "Okc",
     "City": "Oklahoma City",
     "State": "Oklahoma",
-    "Venue": "Chesapeake Energy Arena"
+    "Venue": "Chesapeake Energy Arena",
+    "Id": 18
   },
   {
     "Team": "Trail Blazers",
     "Abbreviation": "Por",
     "City": "Portland",
     "State": "Oregon",
-    "Venue": "Rose Garden"
+    "Venue": "Rose Garden",
+    "Id": 19
   },
   {
     "Team": "Jazz",
     "Abbreviation": "Uta",
     "City": "Salt Lake City",
     "State": "Utah",
-    "Venue": "EnergySolutions Arena"
+    "Venue": "EnergySolutions Arena",
+    "Id": 20
   },
   {
     "Team": "Warriors",
     "Abbreviation": "Gol",
     "City": "Oakland",
     "State": "California",
-    "Venue": "Oracle Arena"
+    "Venue": "Oracle Arena",
+    "Id": 21
   },
   {
     "Team": "Clippers",
     "Abbreviation": "Lac",
     "City": "Los Angeles",
     "State": "California",
-    "Venue": "Staples Center"
+    "Venue": "Staples Center",
+    "Id": 22
   },
   {
     "Team": "Lakers",
     "Abbreviation": "Lal",
     "City": "Los Angeles",
     "State": "California",
-    "Venue": "Staples Center"
+    "Venue": "Staples Center",
+    "Id": 23
   },
   {
     "Team": "Suns",
     "Abbreviation": "Pho",
     "City": "Phoenix",
     "State": "Arizona",
-    "Venue": "US Airways Center"
+    "Venue": "US Airways Center",
+    "Id": 24
   },
   {
     "Team": "Kings",
     "Abbreviation": "Sac",
     "City": "Sacramento",
     "State": "California",
-    "Venue": "Power Balance Pavilion"
+    "Venue": "Power Balance Pavilion",
+    "Id": 25
   },
   {
     "Team": "Mavericks",
     "Abbreviation": "Dal",
     "City": "Dallas",
     "State": "Texas",
-    "Venue": "American Airlines Center"
+    "Venue": "American Airlines Center",
+    "Id": 26
   },
   {
     "Team": "Rockets",
     "Abbreviation": "Hou",
     "City": "Houston",
     "State": "Texas",
-    "Venue": "Toyota Center"
+    "Venue": "Toyota Center",
+    "Id": 27
   },
   {
     "Team": "Grizzlies",
     "Abbreviation": "Mem",
     "City": "Memphis",
     "State": "Tennessee",
-    "Venue": "FedExForum"
+    "Venue": "FedExForum",
+    "Id": 28
   },
   {
     "Team": "Pelicans",
     "Abbreviation": "Nor",
     "City": "New Orleans",
     "State": "Louisiana",
-    "Venue": "New Orleans Arena"
+    "Venue": "New Orleans Arena",
+    "Id": 29
   },
   {
     "Team": "Spurs",
     "Abbreviation": "San",
     "City": "San Antonio",
     "State": "Texas",
-    "Venue": "AT&T Center"
+    "Venue": "AT&T Center",
+    "Id": 30
   }
 ]
-
-module.exports = teamsData;
